53e9ae651d7be0e1272a806a21c644c81fdf0f25
Juliana Reis <julianapriscila.reis@gmail.com>
2026-02-10 02:48:51 -0300

    feat: Implement lead forms with validation and API integration for user inquiries

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function(){
    $('#mobile_btn').on('click', function() {
       $('#mobile_menu').toggleClass('active'); 
       $('#mobile_btn').find('i').toggleClass('fa-regular fa-rectangle-xmark');
